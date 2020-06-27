612fc21aac14c53a70d9f39ea3d1293522090789
Daniel Florescu <daniel.florescu1905@protonmail.ch>
2020-06-27 21:29:01 +0200

    1.3 and 1.4

---
+++ 1.3/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+	float fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+	fahr = lower;
+	printf("Lower bound is %d\nUpper bound is %d\nStep size is %d\n", lower, upper, step);
+	printf("Fahr | Celsius\n");
+	while(fahr < upper){
+		celsius = (5.0/9.0) * (fahr-32.0);
+		printf("%3.0f %6.1f\n", fahr, celsius);
+		fahr = fahr + step;
+	}
+}
---
+++ 1.4/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+	float fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+	celsius = lower;
+	printf("Lower bound is %d\nUpper bound is %d\nStep size is %d\n", lower, upper, step);
+	printf("Celsius | Fahr\n");
+	while(fahr < upper){
+		fahr = (9.0/5.0) * celsius + 32.0;
+		printf("%6.1f %3.0f\n", celsius, fahr);
+		celsius = celsius + step;
+	}
+}
