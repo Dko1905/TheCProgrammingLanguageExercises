e2b6ebf74b161fe73ffc35063e4d7a6a0eb03cf7
Daniel Florescu <daniel.florescu1905@protonmail.ch>
2020-06-29 20:37:54 +0200

    1.20 Done

---
+++ 1.20/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#define MAXLINE 100
+
+int main(){
+	// 8 spaces for one tab
+	// 01234567
+	// |  |   |
+	// |  t   |
+	// 0123
+	//     0123
+	int n = 0, tabspaces = 8, c;
+
+	while((c = getchar()) != EOF){
+		int spaces = tabspaces - (n % tabspaces);
+		if(c == '\t'){
+			for(int i = 0; i <= spaces; ++i){
+				putchar(' ');
+			}
+			n+=spaces+1;
+		}
+		else{
+			if(c == '\n'){
+				n = 0;
+			}
+			putchar(c);
+			++n;
+		}
+	}
+
+	return 0;
+}
